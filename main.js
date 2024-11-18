4996ded5933ce558ed429f443d53d8a14027293d
altkhaN0 <altkhansargent@yandex.com>
2024-11-19 00:24:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Allowlist URL'sini fetch ile al
-    fetch('https://raw.githubusercontent.com/altkhaN0/ds/refs/heads/main/allowlist.json')
+    fetch('https://cdn.jsdelivr.net/gh/altkhaN0/ds@main/allowlist.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error("Failed to load the auth list.");
